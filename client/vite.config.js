d479b0c5781e02c651bedea114eb174e8c79cfd7
DhWalidkhl <sm.walid69@yahoo.com>
2025-11-10 09:47:44 +0600

    relplace setup file

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,18 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ command }) => ({
+// https://vite.dev/config/
+export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./",
-  build: {
-    outDir: "dist",
-  },
   server: {
-    proxy: command === "serve" ? {
-      "/api": {
-        target: "http://localhost:5000",
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
         changeOrigin: true,
-      },
-    } : undefined,
-  },
-}));
+      }
+    }
+  }
+})
